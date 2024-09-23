817411efcc1cb1d319ad3fd063f8c4d9a040829b
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-09-23 14:57:00 +0300

    added discount service

---
+++ Services/Discount/MicroShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,30 @@
+﻿using MicroShop.Discount.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace MicroShop.Discount.Context
+{
+    public class DapperContext : DbContext
+    {
+
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;initial Catalog=MicroShopDiscountApi;integrated Security=true");
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
