7e8c45872efa7b7924c12873748594884d3a65b0
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-10-09 17:12:38 +0300

    identityserver config

--- IdentityServer/MicroShop.IdentityServer/Config.cs
+++ IdentityServer/MicroShop.IdentityServer/Config.cs
@@ -2,57 +2,80 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace MicroShop.IdentityServer
 {
     public static class Config
     {
-        public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
-                   };
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+          new ApiResource("resourceCatalog")
+          {
+              Scopes = {"CatalogFullPermission","CatalogReadPermission"}
+          },
+          new ApiResource("resourceDiscount")
+          {
+              Scopes={"DiscountFullPermission"}
+          },
+          new ApiResource("resourceOrder")
+          {
+              Scopes={"OrderFullPermission"}
+          }
+        };
 
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Email(),
+            new IdentityResources.Profile(),
+        };
+
+        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+        {
+            new ApiScope("CatalogFullPermission","Full authority for catalog operations"),
+            new ApiScope("CatalogReadPermission","Reading authority for catalog operations"),
+            new ApiScope("DiscountFullPermission","Full authority for discount operations"),
+            new ApiScope("OrderFullPermission","Full authority for order operations")
+        };
+
+        public static IEnumerable<Client> Clients => new Client[]
+        {
+            //vistors
+            new Client
             {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
-            };
-
-        public static IEnumerable<Client> Clients =>
-            new Client[]
+                ClientId="MicroShopVisitorId",
+                ClientName="Micro Shop Visitor User",
+                AllowedGrantTypes=GrantTypes.ClientCredentials,
+                ClientSecrets={new Secret("microshopsecret".Sha256())},
+                AllowedScopes={ "CatalogReadPermission" }
+            },
+            //manger
+            new Client
             {
-                // m2m client credentials flow client
-                new Client
-                {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
+                ClientId="MicroShopManagerId",
+                ClientName="Micro Shop Manager User",
+                AllowedGrantTypes=GrantTypes.ClientCredentials,
+                ClientSecrets={new Secret("microshopsecret".Sha256())},
+                AllowedScopes={ "CatalogReadPermission", "CatalogFullPermission" }
 
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "scope1" }
-                },
-
-                // interactive client using code flow + pkce
-                new Client
-                {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.Code,
-
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
-                },
-            };
+            },
+            //admin
+            new Client
+            {
+                ClientId="MicroShopAdminId",
+                ClientName="Micro Shop Admin User",
+                AllowedGrantTypes=GrantTypes.ClientCredentials,
+                ClientSecrets={new Secret("microshopsecret".Sha256())},
+                AllowedScopes={ "CatalogReadPermission", "CatalogFullPermission", "DiscountFullPermission", "OrderFullPermission", IdentityServerConstants.LocalApi.ScopeName,
+                IdentityServerConstants.StandardScopes.Email,
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile},
+                AccessTokenLifetime=800
+            }
+        };
     }
 }
