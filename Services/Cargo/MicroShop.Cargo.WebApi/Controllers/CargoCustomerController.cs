0b91b0e110c8839b81c4221c76382d49462b6ea0
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-10-24 19:49:56 +0300

    CargoCustomer Controller Added

---
+++ Services/Cargo/MicroShop.Cargo.WebApi/Controllers/CargoCustomerController.cs
@@ -0,0 +1,78 @@
+﻿using MicroShop.Cargo.Business.Abstract;
+using MicroShop.Cargo.DtoLayer.DTOs.CargoCustomerDto;
+using MicroShop.Cargo.EntityLayer.Concrate;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroShop.Cargo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoCustomerController : ControllerBase
+    {
+        private readonly ICargoCustomerService _cargoCustomerService;
+
+        public CargoCustomerController(ICargoCustomerService cargoCustomerService)
+        {
+            _cargoCustomerService = cargoCustomerService;
+        }
+
+        [HttpGet]
+        public IActionResult CargoCustomerList()
+        {
+            var values = _cargoCustomerService.TGetAll();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult CargoCustomerGet(int id)
+        {
+            var value = _cargoCustomerService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCargoCostumer(CreateCargoCustomerDto createCargoCustomerDto)
+        {
+            CargoCustomer cargoCustomer = new CargoCustomer()
+            {
+                Address = createCargoCustomerDto.Address,
+                City = createCargoCustomerDto.City,
+                District = createCargoCustomerDto.District,
+                Email = createCargoCustomerDto.Email,
+                Name = createCargoCustomerDto.Name,
+                Phone = createCargoCustomerDto.Phone,
+                Surname = createCargoCustomerDto.Surname
+            };
+            _cargoCustomerService.TInsert(cargoCustomer);
+            return Ok("CargoCustomer Added");
+        }
+
+        [HttpDelete]
+        public IActionResult RemoveCargoCustomer(int id)
+        {
+            _cargoCustomerService.TDelete(id);
+            return Ok("CargoCustomerDeleted");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCargoCustomer(UpdateCargoCustomerDto updateCargoCustomerDto)
+        {
+            CargoCustomer cargoCustomerToUpdate = new CargoCustomer()
+            {
+                Address = updateCargoCustomerDto.Address,
+                City = updateCargoCustomerDto.City,
+                CargoCustomerId = updateCargoCustomerDto.CargoCustomerId,
+                District = updateCargoCustomerDto.District,
+                Email = updateCargoCustomerDto.Email,
+                Name = updateCargoCustomerDto.Name,
+                Phone = updateCargoCustomerDto.Phone,
+                Surname = updateCargoCustomerDto.Surname,
+
+            };
+            _cargoCustomerService.TUpdate(cargoCustomerToUpdate);
+            return Ok("CargoCustomerUpdated");
+        }
+
+    }
+}
