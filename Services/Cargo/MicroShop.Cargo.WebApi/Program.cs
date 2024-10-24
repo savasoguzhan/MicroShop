561476b4858b61ece7d6c1f039ff149f3e8c50ec
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-10-24 20:26:13 +0300

    config for di

--- Services/Cargo/MicroShop.Cargo.WebApi/Program.cs
+++ Services/Cargo/MicroShop.Cargo.WebApi/Program.cs
@@ -1,6 +1,25 @@
+using MicroShop.Cargo.Business.Abstract;
+using MicroShop.Cargo.Business.Concreta;
+using MicroShop.Cargo.DAL.Abstract;
+using MicroShop.Cargo.DAL.Concrate;
+using MicroShop.Cargo.DAL.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<CargoContext>();
+
+builder.Services.AddScoped<ICargoCompanyDal, EFCargoCompanyDal>();
+builder.Services.AddScoped<ICargoCompanyService, CargoCompanyManager>();
+
+builder.Services.AddScoped<ICargoCustomerDal, EFCargoCustomerDal>();
+builder.Services.AddScoped<ICargoCustomerService, CargoCustomerManager>();
+
+builder.Services.AddScoped<ICargoDetailDal, EFCargoDetailDal>();
+builder.Services.AddScoped<ICargoDetailService, CargoDetailManager>();
+
+builder.Services.AddScoped<ICargoOperationDal, EFCargoOperationDal>();
+builder.Services.AddScoped<ICargoOperationService, CargoOperationManager>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
