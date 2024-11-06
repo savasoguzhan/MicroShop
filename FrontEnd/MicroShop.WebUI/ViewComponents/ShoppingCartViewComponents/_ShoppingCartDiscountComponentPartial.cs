da95bb6544383b4eb6d689a1cc6cd8996616a7d6
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-11-06 15:00:56 +0300

    shoppingCartViewComponent

---
+++ FrontEnd/MicroShop.WebUI/ViewComponents/ShoppingCartViewComponents/_ShoppingCartDiscountComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroShop.WebUI.ViewComponents.ShoppingCartViewComponents
+{
+    public class _ShoppingCartDiscountComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
