2639e9969577c5eeec16ef26f3cf6cfc70223883
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-11-06 14:02:34 +0300

    productlistPAge

---
+++ FrontEnd/MicroShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroShop.WebUI.ViewComponents.ProductListViewComponents
+{
+    public class _ProductListComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
