2273d7377be602148191f1d86a663da15919e2e0
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-11-07 13:40:53 +0300

    adminLayoutSidebad ViewComponent

---
+++ FrontEnd/MicroShop.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroShop.WebUI.Areas.Admin.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutHeadComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
