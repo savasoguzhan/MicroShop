c08513d5a7d58222304d0f84d805bc0bc60541b6
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-11-13 12:21:09 +0300

    add product views

---
+++ FrontEnd/MicroShop.WebUI/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using MicroShop.Dto.CatologDtos.ProductDtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MicroShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    [Route("Admin/Product")]
+    public class ProductController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v1 = "Home PAge";
+            ViewBag.v2 = "Product";
+            ViewBag.v3 = "Product Operation";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7200/api/Products");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [Route("CreateProduct")]
+        public async Task<IActionResult> CreateProduct()
+        {
+            return View();
+        }
+
+    }
+}
--- Services/Catalog/MicroShop.Catalog/Controllers/ProductsController.cs
+++ Services/Catalog/MicroShop.Catalog/Controllers/ProductsController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MicroShop.Catalog.Controllers
 {
-    [Authorize]
+    [AllowAnonymous]
     [Route("api/[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
