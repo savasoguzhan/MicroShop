a296a25e7d09fa8c8efe10a869b4c29e4116de0f
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-11-13 13:00:05 +0300

    update and deleted product

--- FrontEnd/MicroShop.Dto/CatologDtos/ProductDtos/UpdateProductDto.cs
+++ FrontEnd/MicroShop.Dto/CatologDtos/ProductDtos/UpdateProductDto.cs
@@ -8,5 +8,17 @@ namespace MicroShop.Dto.CatologDtos.ProductDtos
 {
     public class UpdateProductDto
     {
+        public string ProductID { get; set; }
+
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+
+
+
+        public string ProductImageUrl { get; set; }
+
+        public string ProductDescription { get; set; }
+
+        public string CategoryID { get; set; }
     }
 }
--- FrontEnd/MicroShop.WebUI/Areas/Admin/Controllers/ProductController.cs
+++ FrontEnd/MicroShop.WebUI/Areas/Admin/Controllers/ProductController.cs
@@ -77,5 +77,68 @@ namespace MicroShop.WebUI.Areas.Admin.Controllers
         }
 
 
+        [Route("DeleteProduct/{id}")]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage =await client.DeleteAsync("https://localhost:7200/api/Products?id="+id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Product", new { area = "Admin" });
+            }
+            return View();  
+        }
+
+        [Route("UpdateProduct/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateProduct(string id)
+        {
+
+
+            var client1 = _httpClientFactory.CreateClient();
+            var responseMessge1 = await client1.GetAsync("https://localhost:7200/api/Categories");
+            var jsonData1 = await responseMessge1.Content.ReadAsStringAsync();
+            var values1 = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData1);
+            List<SelectListItem> categoryValue1 = (from x in values1
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID
+                                                  }).ToList();
+
+            ViewBag.CategoryValue = categoryValue1;
+
+
+
+
+
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7200/api/Products/" + id);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateProductDto>(jsonData);
+                return View(values);    
+            }
+            return View();
+        }
+
+
+        [Route("UpdateProduct/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateProductDto);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+
+            var responseMessage = await client.PutAsync("https://localhost:7200/api/Products/", stringContent);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Product", new { area = "Admin" });
+            }
+            return View();
+        }
     }
 }
