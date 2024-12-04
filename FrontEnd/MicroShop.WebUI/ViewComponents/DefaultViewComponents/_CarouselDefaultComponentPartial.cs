e1b81b1a6006abad62c0dd20204d0e6c29be5a9c
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-12-04 13:12:46 +0300

    featureslider MangoDbConsume

--- FrontEnd/MicroShop.WebUI/ViewComponents/DefaultViewComponents/_CarouselDefaultComponentPartial.cs
+++ FrontEnd/MicroShop.WebUI/ViewComponents/DefaultViewComponents/_CarouselDefaultComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MicroShop.Dto.CatologDtos.FeatureSliderDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace MicroShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _CarouselDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CarouselDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessge = await client.GetAsync("https://localhost:7200/api/FeatureSliders");
+            if(responseMessge.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessge
+                    .Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureSliderDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
