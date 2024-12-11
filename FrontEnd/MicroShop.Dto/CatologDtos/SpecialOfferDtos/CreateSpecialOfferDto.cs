2adfef31a1c6018d93864439995e3de8d8527699
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-12-11 16:40:11 +0300

    Special Offer admin api consume

---
+++ FrontEnd/MicroShop.Dto/CatologDtos/SpecialOfferDtos/CreateSpecialOfferDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroShop.Dto.CatologDtos.SpecialOfferDtos
+{
+    public class CreateSpecialOfferDto
+    {
+        
+
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ FrontEnd/MicroShop.Dto/CatologDtos/SpecialOfferDtos/UpdateSpecialOfferDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroShop.Dto.CatologDtos.SpecialOfferDtos
+{
+    public class UpdateSpecialOfferDto
+    {
+        public string SpecialOfferId { get; set; }
+
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
