87904487e2a45827b1a37bbbaa52d3553389db05
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-12-13 15:03:12 +0300

    .

--- FrontEnd/MicroShop.WebUI/ViewComponents/DefaultViewComponents/_SpecialOfferComponentPartial.cs
+++ FrontEnd/MicroShop.WebUI/ViewComponents/DefaultViewComponents/_SpecialOfferComponentPartial.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MicroShop.Dto.CatologDtos.SpecialOfferDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
 
 namespace MicroShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _SpecialOfferComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _SpecialOfferComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async  Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7200/api/SpecialOffers");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSpecialOfferDto>>(jsonData);
+                return View(values);
+            }
+            
             return View();
         }
     }
