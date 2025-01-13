ed190b6e61a49c897811af31e8e5a1d7a15a6afb
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2025-01-13 14:49:32 +0300

    api consume for UI

--- FrontEnd/MicroShop.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
+++ FrontEnd/MicroShop.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MicroShop.Dto.CatologDtos.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace MicroShop.WebUI.ViewComponents.UILayoutViewComponents
 {
     public class _FooterUILayoutComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessge = await client.GetAsync("https://localhost:7200/api/Abouts");
+            if(responseMessge.IsSuccessStatusCode)
+            {
+                var jsonData = await  responseMessge.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
