b8c76762a41ac42a3b845ac9808bf54951f165f0
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2025-01-27 12:55:01 +0300

    //

--- FrontEnd/MicroShop.WebUI/Areas/Admin/Controllers/ProductImageController.cs
+++ FrontEnd/MicroShop.WebUI/Areas/Admin/Controllers/ProductImageController.cs
@@ -1,11 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MicroShop.Dto.CatologDtos.ProductImagesDtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Operations;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace MicroShop.WebUI.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [AllowAnonymous]
+    [Route("Admin/ProductImage")]
     public class ProductImageController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductImageController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        [Route("ProductImageDetail/{id}")]
         public async Task<IActionResult> ProductImageDetail(string id)
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7200/api/ProductImages/ProductImagesByProductId?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateProductImageDto>(jsonData);
+            }
+            return Ok(responseMessage);
+
+        }
+
+        [HttpPost]
+        [Route("ProductImageDetail/{id}")]
+        public async Task<IActionResult> ProductImageDetail(UpdateProductImageDto updateProductImageDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateProductImageDto);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7200/api/ProductImages", stringContent);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ProductListWithCategory", "Product", new { area = "Admin" });
+            }
             return View();
         }
     }
