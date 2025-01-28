f349a2cf0da264a0edb21e302355ff3e6af3efa7
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2025-01-28 17:04:57 +0300

    comments admin area consume

---
+++ FrontEnd/MicroShop.WebUI/Areas/Admin/Controllers/CommentController.cs
@@ -0,0 +1,78 @@
+﻿using MicroShop.Dto.CommentsDtos;
+using MicroShop.WebUI.ViewComponents.UILayoutViewComponents;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MicroShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    [Route("Admin/Comment")]
+    public class CommentController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CommentController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessge = await client.GetAsync("https://localhost:7055/api/Comments");
+            if(responseMessge.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessge.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCommentDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [Route("DeleteComment/{id}")]
+        public async Task<IActionResult> DeleteComment(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessge = await client.DeleteAsync("https://localhost:7055/api/Comments?id=" + id);
+            if (responseMessge.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Comment", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [Route("UpdateComment/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateComment(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7055/api/Comments/" + id);
+            if( responseMessage.IsSuccessStatusCode )
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateCommentDto>(jsonData);
+                return View(values);    
+            }
+            return View();
+        }
+
+        [Route("UpdateComment/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateComment(UpdateCommentDto updateCommentDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateCommentDto);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"applicaton/json");
+            var responseMessage = await client.PutAsync("https://localhost:7055/api/Comments/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Comment", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
