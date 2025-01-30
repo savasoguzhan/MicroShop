9ad2e9861b8cac8c15809793508d1a6bf12437e2
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2025-01-30 18:46:40 +0300

    ui consume for contact

--- FrontEnd/MicroShop.WebUI/Controllers/ContactController.cs
+++ FrontEnd/MicroShop.WebUI/Controllers/ContactController.cs
@@ -1,11 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MicroShop.Dto.CatologDtos.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace MicroShop.WebUI.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public async Task<IActionResult> Index(CreateContactDto createContactDto)
+        {
+            createContactDto.IsRead = false;
+            createContactDto.SendDate = DateTime.Now;
+            var client = _httpClientFactory.CreateClient();
+            var JsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent stringContent = new StringContent(JsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7200/api/Contacts", stringContent);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index","Default");
+            }
             return View();
         }
     }
